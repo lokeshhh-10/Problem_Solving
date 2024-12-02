13012c9e7aa1478e31d6a6ab4d60fc1ef0c3220e
lokesh <lokesh95664@gmail.com>
2024-12-02 23:11:57 +0530

    ....

--- pattern problems/R_sided_tri.py
+++ pattern problems/R_sided_tri.py
@@ -7,7 +7,7 @@
 n = 5
 for i in range(n):
     for j in range(i, n):
-        print("", end=' ')
+        print(" ", end='')
     for k in range(i+1):
         print("*", end=" ")
     print()
--- pattern problems/diamond.py
+++ pattern problems/diamond.py
@@ -1,4 +1,4 @@
-n = 5
+n = 3
 for i in range(n):
     for j in range(i, n-1):
         print(' ', end=' ')
